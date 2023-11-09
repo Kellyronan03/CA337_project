adfebc3d2d650b1a02761e86fe5b51a34b99265b
kellyr96 <ronan.kelly96@mail.dcu.ie>
2023-11-09 18:06:17 +0000

    Assignment 4 completion

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
@@ -10,13 +9,24 @@ def index():
     ''' home page of app '''
     return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    ''' load a model and vocab and apply it to a sample review '''
+    ''' load a model and vocab and apply it to user input review '''
     model = pickle.load(open('ca337-nb1000-model.pkl','rb'))
     features = pickle.load(open('ca337-nb1000-features.pkl','rb'))
-    review = 'I love it'
+
+    if request.method == 'POST':
+        # Get the review from the form
+        review = request.form['review_text']
+    else:
+        # Default review for GET request
+        review = 'I loved it'
+    
     prediction = model.predict(features.transform([review]))[0]
-    return render_template('predict.html',result={'text':review,'prediction':prediction})
+    # Convert numerical prediction to string
+    prediction_text = 'Positive' if prediction == 1 else 'Negative'
+    return render_template('predict.html', result={'text': review, 'prediction': prediction_text})
+
 
 app.run(host='0.0.0.0', port=8000)
